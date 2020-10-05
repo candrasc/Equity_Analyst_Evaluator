dc2f1702eaeee07a1b50c18413c20dcf4e2ba084
candrasc <charles.andrascik5@gmail.com>
2020-10-04 21:06:38 -0400

    Fixed bug note allowing buy/sell recos to plot

--- flask_app/stock_analyser.py
+++ flask_app/stock_analyser.py
@@ -13,8 +13,7 @@ def get_ticker(symbol):
 
 def get_recos(ticker):
     df_base = ticker.recommendations.reset_index()
-    df = df_base.copy()
-    return df
+    return df_base
 
 def get_prices(symbol):
     symbol = symbol
@@ -28,10 +27,12 @@ def standardize_recos(df):
     df['To Grade'] = np.where((df['To Grade'].isin(['Neutral','Market Perform','Equal-Weight', 'Sector Perform','Perform','Equal-weight','In-Line','Peer Perform','Sector Weight'])),'Hold',df['To Grade'])
     #Change terms to Sell
     df['To Grade'] = np.where((df['To Grade'].isin(['Underperform','Underweight','Reduce','Negative'])),'Sell',df['To Grade'])
+    #Fix date time to not include seconds
+    df['Date'] = df['Date'].values.astype('<M8[M]')
     return df
 
 def remove_bad_recos(df):
-    df_new = df.loc[(df['To Grade'] == 'Hold')|(df['To Grade']=='Buy')|(df['To Grade']=='Sell')].copy()
+    df = df.loc[(df['To Grade'] == 'Hold')|(df['To Grade']=='Buy')|(df['To Grade']=='Sell')]
     return df
 
 #This function combines all above functions to get the formatted dataframes for plotting
@@ -40,16 +41,50 @@ def get_frames(symbol):
     df_recos = get_recos(ticker)
     df_prices = get_prices(symbol)
 
-    std_recos = standardize_recos(df_recos)
-    std_recos = remove_bad_recos(std_recos)
-    return std_recos, df_prices
+    df_recos = standardize_recos(df_recos)
+    df_recos= remove_bad_recos(df_recos)
+    return df_recos, df_prices
+"""
+Now we have all the required functions to retrieve the required data.
 
-def format_plot():
-    pass
-    #Coming soon
+The below functions will be focused on formatting and plotting
+"""
+
+#Merges the recos and prices dfs for plotting and analysis
+def format_dfs(df_recos, df_prices):
+
+    df_buy = df_recos.loc[df_recos['To Grade']=='Buy']
+    df_sell = df_recos.loc[df_recos['To Grade']=='Sell']
+
+    plot_buy = df_prices.merge(df_buy,on='Date', how = 'left')
+    plot_sell = df_prices.merge(df_sell,on='Date', how = 'left')
+
+    plot_buy = plot_buy.loc[plot_buy['To Grade']=='Buy']
+    plot_sell = plot_sell.loc[plot_sell['To Grade']=='Sell']
+    return plot_buy, plot_sell
+
+#Current issue is that date axis is fucked up
+def line_plot(prices, plot_buy, plot_sell, symbol):
+    fig = plt.figure(figsize = (15,5))
+    plt.style.use('ggplot')
+
+    #variables for plot to make tweaking easier
+    line_alpha = 0.6
+    marker_alpha = 1
+    line_color = 'b'
+    buy_color = 'g'
+    sell_color = 'r'
+    marker_s = 70
+
+    ax = plt.subplot(1,1,1)
+    plt.plot(prices.Date, prices['Adj Close'], color =line_color, alpha=line_alpha)
+    plt.scatter(x = plot_buy.Date, y = plot_buy['Adj Close'], color = buy_color, marker = '^', label = 'Buy Reco',s=marker_s, alpha = marker_alpha)
+    plt.scatter(x = plot_sell.Date, y = plot_sell['Adj Close'], color = sell_color, marker = 'v', label = 'Sell Reco',s=marker_s, alpha=marker_alpha)
+    plt.title('{} Buy and Sell Recommendations'.format(symbol))
+    plt.legend(loc=2)
+    return plt.show()
 
 def plot(symbol):
-    std_recos, df_prices = get_frames(symbol)
-    plt.plot(df_prices.Date, df_prices['Adj Close'])
-    plt.title("{} Historical Prices".format(symbol))
-    plt.show()
+    df_recos, df_prices = get_frames(symbol)
+    plot_buy, plot_sell = format_dfs(df_recos,df_prices)
+    return line_plot(df_prices,plot_buy,plot_sell,symbol)
